0f7aa2783df4e4d8760ae4d0d55920ec417766cc
Marcelo Sampaio <marcelo@artlogica.com.br>
2014-03-22 22:57:11 -0300

    Initial Commit

---
+++ peerToPeer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  peerToPeer
+//
+//  Created by Marcelo Sampaio on 3/22/14.
+//  Copyright (c) 2014 Marcelo Sampaio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ peerToPeer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  peerToPeer
+//
+//  Created by Marcelo Sampaio on 3/22/14.
+//  Copyright (c) 2014 Marcelo Sampaio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
